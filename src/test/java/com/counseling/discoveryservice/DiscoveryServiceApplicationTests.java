1aaf3533c5c896998331f6e462da3898292af326
Prakash <prakashadhikarijhapali@gmail.com>
2020-09-26 00:24:52 +1000

    discovery server created

---
+++ src/test/java/com/counseling/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counseling.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
